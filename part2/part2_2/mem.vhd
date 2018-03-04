3b7907c175ffccfad4b4e2a2e7a3a8f9dd0ab181
Daniel Limanowski <dlimanow@iastate.edu>
2018-03-04 14:33:37 -0600

    complete instruction mem testbench (proj2_2)

--- part2/part2_2/mem.vhd
+++ part2/part2_2/mem.vhd
@@ -1,27 +1,26 @@
 -- Quartus Prime VHDL Template
 -- Single-port RAM with single read/write address
 
+-- NOTE: To use the memory module, you must load it into modelsim using the
+-- following command during simulation
+-- mem load -infile dmem.hex -format hex
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity mem is
-
 	generic
 	(
 		DATA_WIDTH : natural := 32;
 		ADDR_WIDTH : natural := 10
 	);
 
-	port
-	(
-		clk		: in std_logic;
-		addr	: in natural range 0 to 2**ADDR_WIDTH - 1;
-		data	: in std_logic_vector((DATA_WIDTH-1) downto 0);
-		we		: in std_logic := '1';
-		q		: out std_logic_vector((DATA_WIDTH-1) downto 0)
-	);
-
+	port ( clk	: in std_logic;
+		   addr	: in natural range 0 to 2**ADDR_WIDTH - 1;
+		   data	: in std_logic_vector((DATA_WIDTH-1) downto 0);
+		   we	: in std_logic := '1';
+		   q	: out std_logic_vector((DATA_WIDTH-1) downto 0) );
 end mem;
 
 architecture rtl of mem is
