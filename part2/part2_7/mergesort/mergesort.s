91637bd17653f321619e9e05ad5ab8cc0b5697b8
Daniel Limanowski <dlimanow@iastate.edu>
2018-03-29 11:07:42 -0500

    fix bugs in alu and single cycle + organize tests

--- part2/part2_7/mergesort/mergesort.s
+++ part2/part2_7/mergesort/mergesort.s
@@ -1,158 +1,117 @@
-# Original author: jmc734
-# Modified by: Joel16
+# Merge Sort
+# Joel16
 
-.data
-# Array contents
-eight:	.word	8                    # 1st element
-five:	.word	5                    # 2nd element
-four:	.word	4                    # 3rd element
-nine:	.word	9                    # 4th element
-
-# An array of pointers (indirect array)
-length:	.word	4                    # Array length
-array:	.word	eight
-	.word	five
-	.word	four
-	.word	nine
-
+# Some references:
+# $4 - $5 = $a0 - $a1
+# $8 - $15 = $t0 - $t7
+# $16 - $19 = $s0 - $s3
+# $24 - $25 = $t8-$t9
 .text
 .globl main
 
 main:
-	la $a0, array                # Load the start address of the array
-	lw $t0, length               # Load the array length
-	sll $t0, $t0, 2              # Multiple the array length by 4 (the size of the elements)
-	add $a1, $a0, $t0            # Calculate the array end address
-	jal mergesort                # Call the merge sort function
-  	beq $0, $0, sortend          # We are finished sorting
-	
-##
-# Recrusive mergesort function
-#
-# @param $a0 first address of the array
-# @param $a1 last address of the array
-##
-mergesort:
-	addi $sp, $sp, -16           # Adjust stack pointer
-	sw $ra, 0($sp)               # Store the return address on the stack
-	sw $a0, 4($sp)               # Store the array start address on the stack
-	sw $a1, 8($sp)               # Store the array end address on the stack
-	
-	sub $t0, $a1, $a0            # Calculate the difference between the start and end address (i.e. number of elements * 4)
+	addi $4, $0, 8
 
-	addi $t9, $0, 4
-	slt $at, $t9, $t0
-	beq $at, $0, mergesortend    # If the array only contains a single element, just return 
-	
-	srl $t0, $t0, 3              # Divide the array size by 8 to half the number of elements (shift right 3 bits)
-	sll $t0, $t0, 2              # Multiple that number by 4 to get half of the array size (shift left 2 bits)
-	add $a1, $a0, $t0            # Calculate the midpoint address of the array
-	sw $a1, 12($sp)              # Store the array midpoint address on the stack
-	
-	jal mergesort                # Call recursively on the first half of the array
-	
-	lw $a0, 12($sp)              # Load the midpoint address of the array from the stack
-	lw $a1, 8($sp)               # Load the end address of the array from the stack
-	
-	jal mergesort                # Call recursively on the second half of the array
-	
-	lw $a0, 4($sp)               # Load the array start address from the stack
-	lw $a1, 12($sp)              # Load the array midpoint address from the stack
-	lw $a2, 8($sp)               # Load the array end address from the stack
-	
-	jal merge                    # Merge the two array halves
-	
-mergesortend:
-	lw $ra, 0($sp)               # Load the return address from the stack
-	addi $sp, $sp, 16            # Adjust the stack pointer
-	jr $ra                       # Return 
-	
-##
-# Merge two sorted, adjacent arrays into one, in-place
-#
-# @param $a0 First address of first array
-# @param $a1 First address of second array
-# @param $a2 Last address of second array
-##
-merge:
-	addi $sp, $sp, -16           # Adjust the stack pointer
-	sw $ra, 0($sp)               # Store the return address on the stack
-	sw $a0, 4($sp)               # Store the start address on the stack
-	sw $a1, 8($sp)               # Store the midpoint address on the stack
-	sw $a2, 12($sp)              # Store the end address on the stack
-	
-	addiu $s0, $a0, 0            # Create a working copy of the first half address
-	addiu $s1, $a1, 0            # Create a working copy of the second half address           
-	
-mergeloop:
-	lw $t0, 0($s0)               # Load the first half position pointer
-	lw $t1, 0($s1)               # Load the second half position pointer
-	lw $t0, 0($t0)               # Load the first half position value
-	lw $t1, 0($t1)               # Load the second half position value
-	
-	slt $at, $t0, $t1
-	bne $at, $0, noshift         # If the lower value is already first, don't shift
-	
-	addiu $a0, $s1, 0            # Load the argument for the element to move
-	addiu $a1, $s0, 0            # Load the argument for the address to move it to         
-	jal shift                    # Shift the element to the new position 
-	
-	addi $s1, $s1, 4             # Increment the second half index
+	# load values into memory
+	addi $8, $0, 5
+	sw   $8, 0($4)
+	add $14, $0, $8  # $t6 holds unsorted element 1
 
-noshift:
-	addi $s0, $s0, 4             # Increment the first half index
-	
-	lw $a2, 12($sp)              # Reload the end address
-	slt $at, $s0, $a2
-	beq $at, $0, mergeloopend    # End the loop when both halves are empty 
-	slt $at, $s1, $a2
-	beq $at, $0, mergeloopend    # End the loop when both halves are empty
-	beq $0, $0, mergeloop
-	
-mergeloopend:
-	lw $ra, 0($sp)               # Load the return address
-	addi $sp, $sp, 16            # Adjust the stack pointer
-	jr $ra                       # Return
+	addi $8, $0, 1
+	sw   $8, 4($4)
+	add $15, $0, $8  # $t7 holds unsorted element 2
 
-##
-# Shift an array element to another position, at a lower address
-#
-# @param $a0 address of element to shift
-# @param $a1 destination address of element
-##
-shift:
-	addiu $t0, $0, 10
-	slt $at, $a1, $a0
-	beq $at, $0, shiftend        # If we are at the location, stop shifting
-	addi $t6, $a0, -4            # Find the previous address in the array
-	lw $t7, 0($a0)               # Get the current pointer
-	lw $t8, 0($t6)               # Get the previous pointer
-	sw $t7, 0($t6)               # Save the current pointer to the previous address
-	sw $t8, 0($a0)               # Save the previous pointer to the current address
-	addiu $a0, $t6, 0            # Shift the current position back	
-	beq $0, $0, shift            # Loop again
+	addi $8, $0, 4
+	sw   $8, 8($4)
+	add $24, $0, $8  # $t8 holds unsorted element 3
 
-shiftend:
-	jr $ra                       # Return
+	addi $8, $0, 2
+	sw   $8, 12($4)
+	add $25, $0, $8  # $t6 holds unsorted element 4
 
-# Sorting is complete store sorted numbers in registers t2-t5
-sortend:
-	# Sorted element 1
-	addi $t0, $0, 0
-	lw $t1, array($t0)
-	lw $t2, 0($t1)
-	
-	# Sorted element 2
-	addi $t0, $0, 4
-	lw $t1, array($t0)
-	lw $t3, 0($t1)
+	addi $4, $4, 0   # pointer to original array
+	addi $5, $4, 20  # pointer to the sorted array
 
-	# Sorted element 3
-	addi $t0, $0, 8
-	lw $t1, array($t0)
-	lw $t4, 0($t1)
+	# begin sorting
 
-	# Sorted element 4
-	addi $t0, $0, 12
-	lw $t1, array($t0)
-	lw $t5, 0($t1)
+	# 5 1 4 2
+	lw $8, 0($4)
+	add $0, $0, $0    # nop
+	lw $9, 4($4)
+	add $0, $0, $0    # nop
+	slt $10, $8, $9   # if (5 < 1) set $10 to 1
+	beq $10, $0, swap_1 # stops here mips_single_cycle.vhd 148 Architecture structure
+	j next_1
+
+swap_1:
+	sw $8 4($4)
+	sw $9 0($4)
+
+next_1:
+	# 1 5 4 2
+	lw $8, 8($4)
+	add $0, $0, $0    # nop
+	lw $9, 12($4)
+	add $0, $0, $0    # nop
+	slt $10, $8, $9   # if (4 < 2) set $10 to 1
+	beq $10, $0, swap_2
+	j next_2
+
+swap_2:
+	sw $8 12($4)
+	sw $9 8($4)
+
+next_2:
+    # Final sort
+    # $8 = left pointer, $9 = right pointer, $10 = sorted array pointer
+	# 1 5    2 4
+
+	addi $10, $5, 0                   # new array pointer
+	addi $16, $0, 3                   # size
+
+	addi $8, $4, 0                    # left pointer
+	addi $9, $4, 8                    # right pointer
+
+loop:
+	beq $16, $0, storeLastElement
+	lw $11, 0($8)                     # dereference left
+	add $0, $0, $0                    # nop
+	lw $12, 0($9)	                  # dereference right
+	add $0, $0, $0                    # nop
+	slt $13, $11, $12                 # if (1 < 2) set $13 to 1
+	beq $13, $0, store_right
+	sw $11, 0($10)                    # store left elemnt first
+	addi $8, $8, 4                    # left++
+	j next_3
+
+store_right:
+	sw $12, 0($10)                    # store right element first
+	addi $9, $9, 4                    # right++
+
+next_3:
+	addi $10, $10, 4                  # increment new array pointer
+	addi $16, $16, -1                 # decrement size TODO: make sure this is doing the right thing in the simulation!!1
+	j loop
+
+storeLast_rightHalf:
+	sw $12, 0($10)
+	j done
+
+storeLast_leftHalf:
+	sw $11, 0($10)
+	j done
+
+storeLastElement:
+	slt $13, $11, $12                 # if (1 < 2) set $13 to 1
+	beq $13, $0, storeLastElement_left
+	sw $12, 0($10)                    # store right elemnt first
+	j done
+
+storeLastElement_left:
+	sw $11, 0($10)                    # store left element first
+
+done:
+	lw $16, 0($5)   # registers $16-$19 hold the final merge-sorted list
+	lw $17, 4($5)
+	lw $18, 8($5)
+	lw $19, 12($5)
